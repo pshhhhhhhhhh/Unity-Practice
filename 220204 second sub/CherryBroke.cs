8a642c140a930c18f34e7461efac3387fd7a189d
pshhhhhhhhhh <97819304+pshhhhhhhhhh@users.noreply.github.com>
2022-02-04 15:10:41 +0900

    Add files via upload

---
+++ 220204 second sub/CherryBroke.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryBroke : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision) //콜라이더,리지드바디가 적용된 오브젝트와 충돌하면 적용
+    {
+        
+        Destroy(this.gameObject); //오브젝트 삭제하기
+        
+    }
+}
